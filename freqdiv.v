d38fccd50037ca45b8cbac0feb280e8140d66670
MortezaSaraf <saraf.neuroscience@gmail.com>
2015-11-01 20:45:14 -0500

    Update freqdiv.v
    Hey Jon, 
    
    Last file was simulated, but there was a problem with synthesize. This code is fine in both simulation and synthesize.
    
    Best regards,
    Morteza

--- freqdiv.v
+++ freqdiv.v
@@ -17,27 +17,43 @@ module freqdiv(out, clk, reset);
     reg [31:0] ncount = 32'b0;
     reg [31:0] count = 32'b0;
         
-    always @ (posedge clk)
-        if (reset)
-            pcount <= 0;
-        else if (pcount == N-1)
-            pcount <= 0;
-        else
-            pcount <= pcount + 1;
+//    always @ (posedge clk)
+//        if (reset)
+//            pcount <= 0;
+//        else if (pcount == N-1)
+//            pcount <= 0;
+//        else
+//            pcount <= pcount + 1;
 
-    always @ (negedge clk)
-        if (reset)
-            ncount <= 0;
-        else if (ncount == N-1)
-            ncount <= 0;
-        else
-            ncount <= ncount + 1;
+//    always @ (negedge clk)
+//        if (reset)
+//            ncount <= 0;
+//        else if (ncount == N-1)
+//            ncount <= 0;
+//        else
+//            ncount <= ncount + 1;
 
+	always @ (clk) 
+	 begin
+	    ncount <= ncount + 1;
+	  if (reset)
+	   begin
+            ncount <= 0;
+            out <= 1'b0;
+	   end
+	  else if (ncount <= N-1) 
+            out <= 1'b1;
+          else if (ncount < 2*N-1) 
+	    out <= 1'b0;
+	  else
+            ncount <= 0;
+	 end
+   
     // Slow positive edge period counter
 	 //always @ (posedge clk or negedge clk) begin
-	 always @ (clk) begin
-			count <= pcount + ncount;
-			out <= count < N-1 && !reset;
-	 end
+//	 always @ (clk) begin
+//			count <= pcount + ncount;
+//			out <= count < N-1 && !reset;
+//	 end
 
 endmodule
